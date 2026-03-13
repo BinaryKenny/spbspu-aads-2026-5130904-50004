093eb8f2114d602b00eb4567c6e3b14c3efb38ba
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-13 22:05:40 +0300

    I added the interface for const-iterators

---
+++ khairullin.vadim/S1/CLIter.hpp
@@ -0,0 +1,17 @@
+#ifndef CLITER_HPP
+#define CLITER_HPP
+
+#include "List.hpp"
+template<class T>
+struct CLIter
+{
+  friend class List<T>;
+  List<T> * list;
+  CLIter(List<T> * h);
+  static CLIter begin(List<T> * head);
+  static CLIter end();
+  bool hasNext();
+  bool not_empty();
+  CLIter next();
+  T & value();
+};

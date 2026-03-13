0b0f5bcd867ab96347564f1671680ab2e8d9ff4f
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-13 22:00:13 +0300

    I added the methods for List's iterator

--- khairullin.vadim/S1/LIter.hpp
+++ khairullin.vadim/S1/LIter.hpp
@@ -5,16 +5,16 @@
 template<class T>
 struct LIter
 {
-    friend class List<T>;
-    List<T> * list;
-    LIter(List<T> * h);
-    static LIter begin(List<T> * head);
-    static LIter end();
-    bool hasNext();
-    bool not_empty();
-    LIter next();
-    T & value();
-    LIter insert_value(T & value);
+  friend class List<T>;
+  List<T> * list;
+  LIter(List<T> * h);
+  static LIter begin(List<T> * head);
+  static LIter end();
+  bool hasNext();
+  bool not_empty();
+  LIter next();
+  T & value();
+  LIter insert_value(T & value);
 };
 
 #endif
--- khairullin.vadim/S1/List.hpp
+++ khairullin.vadim/S1/List.hpp
@@ -4,16 +4,16 @@
 template <class T>
 struct List
 {
-    T val;
-    List<T> * next;
-    List<T>(const T& val, List<T> * n);
-    List<T> * add(const T& val, List<T> * n);
-    List<T> * insert(const T& val, List<T> * n);
-    List<T> * cut(List<T> * h);
-    List<T> * erase(List<T> * h);
-    List<T> * clear(List<T> * h);
-    static List<T> * fake(List<T> * h);
-    List<T> * cut_fake(List<T> * fake);
+  T val;
+  List<T> * next;
+  List<T>(const T& val, List<T> * n);
+  List<T> * add(const T& val, List<T> * n);
+  List<T> * insert(const T& val, List<T> * n);
+  List<T> * cut(List<T> * h);
+  List<T> * erase(List<T> * h);
+  List<T> * clear(List<T> * h);
+  static List<T> * fake(List<T> * h);
+  List<T> * cut_fake(List<T> * fake);
 };
 
 #endif

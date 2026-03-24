2ca0b781667d35fb2eb9a2053726077e1a1f954e
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-24 21:13:41 +0300

    I added the interface for List

--- khairullin.vadim/S2/main.cpp
+++ khairullin.vadim/S2/main.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
+
+template <class T>
+struct List
+{
+  T val;
+  List<T> * next, * prev;
+  List<T>(const T& val, List<T> * n, List<T> * b);
+  List<T> * add(const T& val, List<T> * n, List<T> * b);
+  List<T> * insert(const T& val, List<T> * h);
+  List<T> * cut(List<T> * h) noexcept;
+  List<T> * erase(List<T> * h) noexcept;
+  List<T> * clear(List<T> * h);
+  void swap(List & other);
+  List(List & other);
+  List & operator=(List & other);
+  List(List && other);
+  List & operator=(List && other);
+  ~List() = default;
+};
+
 int main(int argc, char ** argv)
 {}

2d08d25cab6f1b922e37c3a84ebbf9aa45c06ee2
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-25 18:57:46 +0300

    I added the classes Queue and Stack

--- khairullin.vadim/S2/main.cpp
+++ khairullin.vadim/S2/main.cpp
@@ -175,5 +175,296 @@ class Queue
     List<T> * tail = nullptr;
 };
 
+template<class T>
+Queue<T>::Queue(const T & rhs):
+head(new List<T>(rhs, nullptr, nullptr)),
+tail(head)
+{}
+
+template<class T>
+bool Queue<T>::not_empty() const
+{
+  return head;
+}
+
+template<class T>
+const T & Queue<T>::value() const
+{
+  return head->val;
+}
+
+template<class T>
+void Queue<T>::swap(Queue<T> & other) noexcept
+{
+  std::swap(head, other.head);
+  std::swap(tail, other.tail);
+}
+
+template<class T>
+Queue<T>::Queue(Queue<T> & other):
+head(nullptr),
+tail(nullptr)
+{
+  if (other.not_empty())
+  {
+    try
+    {
+      head = new List<T>(*other.head);
+      tail = head;
+      while (tail->next)
+      {
+        tail = tail->next;
+      }
+    }
+    catch(...)
+    {
+      delete head;
+    }
+  }
+}
+
+template<class T>
+Queue<T> & Queue<T>::operator=(Queue<T> & other)
+{
+  if (this != & other)
+  {
+    try
+    {
+      Queue<T> temp(other);
+      swap(temp);
+    }
+    catch(...)
+    {
+      throw;
+    }
+  }
+  return *this;
+}
+
+template<class T>
+Queue<T>::Queue(Queue<T> && other) noexcept:
+head(nullptr),
+tail(nullptr)
+{
+  head = std::move(other.head);
+  tail = std::move(other.tail);
+  other.head = nullptr;
+  other.tail = nullptr;
+}
+
+template<class T>
+Queue<T> & Queue<T>::operator=(Queue<T> && other) noexcept
+{
+  if (this != &other)
+  {
+    Queue<T> temp(std::move(other));
+    swap(temp);
+  }
+  return *this;
+}
+
+template<class T>
+void Queue<T>::push(const T & rhs)
+{
+  try
+  {
+    if (tail)
+    {
+      tail = tail->insert(rhs, tail);
+    }
+    else
+    {
+      head = new List<T>(rhs, nullptr, nullptr);
+      tail = head;
+    }
+  }
+  catch(...)
+  {
+    head->clear(head);
+    throw std::bad_alloc();
+  }
+}
+
+template<class T>
+T Queue<T>::drop()
+{
+  try
+  {
+    if (!head)
+    {
+      throw std::logic_error("The queue is empty");
+    }
+    T val = head->val;
+    List<T> * next = head->next;
+    delete head;
+    head = next;
+    if (!head)
+    {
+      tail = nullptr;
+    }
+    else
+    {
+      head->prev = nullptr;
+    }
+    return val;
+  }
+  catch(...)
+  {
+    head->clear(head);
+    throw;
+  }
+}
+
+template<class T>
+Queue<T>::~Queue()
+{
+  if (head)
+  {
+     head->clear(head);
+  }
+}
+
+template <class T>
+class Stack
+{
+public:
+  void push(const T & rhs);
+  T drop();
+  const T & value() const;
+  void swap(Stack & other);
+  bool not_empty() const;
+  Stack() = default;
+  Stack(const T & rhs);
+  Stack(Stack & other);
+  Stack & operator=(Stack & other);
+  Stack(Stack && other) noexcept;
+  Stack & operator=(Stack && other) noexcept;
+  ~Stack();
+private:
+  List<T> * head = nullptr;
+};
+
+template<class T>
+Stack<T>::Stack(Stack<T> & other):
+head(nullptr)
+{
+  if (other.not_empty())
+  {
+    try
+    {
+      head = new List<T>(other.head->val, nullptr, nullptr);
+      List<T> * tail = head;
+      List<T> * copy = other.head->prev;
+      while (copy)
+      {
+        tail->prev = new List<T>(copy->val, tail, nullptr);
+        tail = tail->prev;
+        copy = copy->prev;
+      }
+    }
+    catch(...)
+    {
+      while (head)
+      {
+        List<T> * prev = head->prev;
+        delete head;
+        head = prev;
+      }
+      throw std::bad_alloc();
+    }
+  }
+}
+
+template<class T>
+Stack<T> & Stack<T>::operator=(Stack<T> & other)
+{
+  if (this != &other)
+  {
+     Stack<T> temp(other);
+     swap(temp);
+  }
+  return *this;
+}
+
+template<class T>
+Stack<T>::Stack(Stack<T> && other) noexcept:
+head(other.head)
+{
+    other.head = nullptr;
+}
+
+template<class T>
+Stack<T> & Stack<T>::operator=(Stack<T> && other) noexcept
+{
+  if (this != &other)
+  {
+    Stack<T> temp(std::move(other));
+    swap(temp);
+  }
+  return *this;
+}
+
+template<class T>
+Stack<T>::~Stack()
+{
+  while (head)
+  {
+    List<T> * prev = head->prev;
+    delete head;
+    head = prev;
+    if (head)
+    {
+      head->next = nullptr;
+    }
+  }
+}
+
+template<class T>
+void Stack<T>::push(const T & rhs)
+{
+  try
+  {
+    if (head)
+    {
+      head = head->insert(rhs, head);
+    }
+    else
+    {
+      head = new List<T>(rhs, nullptr, nullptr);
+    }
+  }
+  catch(...)
+  {
+    throw std::bad_alloc();
+  }
+}
+
+template<class T>
+T Stack<T>::drop()
+{
+  try
+  {
+    if (!head)
+    {
+      throw std::logic_error("The stack is empty");
+    }
+    T val = head->val;
+    List<T> * prev = head->prev;
+    delete head;
+    head = prev;
+    if (head)
+    {
+      head->next = nullptr;
+    }
+    return val;
+  }
+  catch(...)
+  {
+    head->clear(head);
+    throw;
+  }
+}
+
+
 int main(int argc, char ** argv)
 {}

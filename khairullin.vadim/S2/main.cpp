056c718ad826909a3eaf908626ade3d88d2a51c5
BinaryKenny <khairullinvadim2006@gmail.com>
2026-04-10 18:51:46 +0300

    I fixed the check for multiply

--- khairullin.vadim/S2/main.cpp
+++ khairullin.vadim/S2/main.cpp
@@ -123,8 +123,11 @@ int main(int argc, char ** argv)
           int_ll val_1 = res_stack.drop().value();
           int_ll val_2 = res_stack.drop().value();
           int_ll result = 0;
-          bool can_multiply = ((val_1 > 0 && val_2 > 0) && (MAX / val_1 >= val_2)) || ((val_1 < 0 and val_2 < 0) && (MIN / (-val_1) <= val_2)) || (val_1 == 0 || val_2 == 0);
-          can_multiply = can_multiply || (((val_1 > 0 && val_2 < 0) && (MIN / val_1 <= val_2)) || (((val_1 < 0 && val_2 > 0) && (MIN/ val_1 >= val_2)) || (val_1 == 0 || val_2 == 0)));
+          bool can_multiply = ((val_1 > 0 && val_2 > 0) && (MAX / val_1 >= val_2)) || (val_1 == 0 || val_2 == 0);
+          can_multiply = can_multiply || ((val_1 < 0 and val_2 < 0) && (MIN / (-val_1) <= val_2));
+          can_multiply = can_multiply || (val_1 > 0 && val_2 < 0) && (MIN / val_1 <= val_2);
+          can_multiply = can_multiply || ((val_1 < 0 && val_2 > 0) && (MIN/ val_1 >= val_2));
+          can_multiply = can_multiply || (val_1 == 0 || val_2 == 0);
           if (can_multiply)
           {
             result = val_1 * val_2;

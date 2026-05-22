7b662ee2f882514a07ba91d0a945f5f533e18552
BinaryKenny <khairullinvadim2006@gmail.com>
2026-05-22 16:29:31 +0300

    I fixed some tests

--- khairullin.vadim/S4/Test.cpp
+++ khairullin.vadim/S4/Test.cpp
@@ -32,8 +32,10 @@ BOOST_AUTO_TEST_CASE(main_test)
   BOOST_TEST(tree->get(3) == "hello");
   auto beg = iterator.begin();
   auto end = iterator.end();
-  BOOST_TEST(beg.read() == std::pair<size_t, std::string>(1, ""));
-  BOOST_TEST(end.read() == std::pair<size_t, std::string>(14, ""));
+  std::pair< size_t, std::string> expected1 = std::make_pair(1,"");
+  std::pair< size_t, std::string> expected2 = std::make_pair(14,"");
+  BOOST_TEST(beg.read() == expected1);
+  BOOST_TEST(end.read() == expected2);
   BOOST_TEST(tree->height() == 5);
   BOOST_TEST(tree->height(beg.root) == 1);
   BOOST_TEST((tree->fallLeft())->data.first == 1);

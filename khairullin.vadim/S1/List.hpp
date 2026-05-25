958d9b9b814d165934b648482085c024e3d3741b
BinaryKenny <khairullinvadim2006@gmail.com>
2026-05-25 16:28:22 +0300

    Start of rewriting this work due to fatal mistake

--- khairullin.vadim/S1/List.hpp
+++ khairullin.vadim/S1/List.hpp
@@ -1,21 +1,387 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include "Node.h"
+#include <iostream>
+#include <stdexcept>
 
-namespace khairullin
-{
-  template <class T>
-  struct List
-  {
-    T val;
-    List * next;
-    List(const T& val, List * n);
-    List * add(const T& val, List * n);
-    List * insert(const T& val, List * n);
-    List * cut(List * h);
-    List * erase(List * h);
-    List * clear(List * h);
-    static List * fake(List * h);
-    List * cut_fake(List * fake);
+namespace khairullin{
+
+    template< class T >
+  struct ConstListIterator;
+  template< class T >
+  struct ListIterator;
+
+  template< class T >
+  struct List {
+    List();
+    ~List();
+    List(const List &other);
+    List & operator=(const List &other);
+    List(List &&other) noexcept;
+    List & operator=(List &&other);
+    void addBegin(const T &val);
+    void addEnd(const T &val);
+    void insert(const T &val, const T &after);
+    void cut(const T &val);
+    void clear();
+    ConstListIterator< T > сbegin() const;
+    ConstListIterator< T > сend() const;
+    ListIterator< T > begin() const;
+    ListIterator< T > end() const;
+    void swap(List &other);
+    void show();
+    private:
+      Node< T > *fake = static_cast< Node< T > * >(::operator new[](sizeof(Node< T >)));
+      Node< T > *head = nullptr;
   };
+
+  template< class T >
+  struct ListIterator {
+    ListIterator();
+    ListIterator(Node< T > * curr);
+    ~ListIterator() = default;
+    ListIterator(const ListIterator & other);
+    ListIterator & operator=(const ListIterator & other);
+    ListIterator(ListIterator && other);
+    ListIterator & operator=(ListIterator && other);
+    ListIterator operator++();
+    ListIterator operator++(int);
+    T operator*();
+    void insert(const T &val);
+    bool operator==(const ListIterator & other);
+    bool operator!=(const ListIterator & other);
+    private:
+      Node< T > * current;
+  };
+
+  template< class T >
+  struct ConstListIterator {
+    ConstListIterator();
+    ConstListIterator(Node< T > * curr);
+    ~ConstListIterator() = default;
+    ConstListIterator(const ConstListIterator & other);
+    ConstListIterator & operator=(const ConstListIterator & other);
+    ConstListIterator(ConstListIterator && other);
+    ConstListIterator & operator=(ConstListIterator && other);
+    ConstListIterator operator++();
+    ConstListIterator operator++(int);
+    T operator*();
+    bool operator==(const ConstListIterator & other);
+    bool operator!=(const ConstListIterator & other);
+    private:
+      Node< T > * current;
+  };
+}
+
+template< class T >
+void khairullin::List< T >::show() {
+  auto curr = head;
+  while (curr) {
+    std::cout << curr->val << "\n";
+    curr = curr->next;
+  }
+}
+
+template< class T >
+khairullin::List< T >::List() {
+  fake->next = head;
+}
+
+template< class T >
+khairullin::List< T >::~List() {
+  clear();
+}
+
+template< class T >
+khairullin::List< T >::List(const List & other) {
+  Node< T > * beg = other.head;
+  while (beg) {
+    addEnd(beg->val);
+    beg = beg->next;
+  }
+}
+
+template< class T >
+khairullin::List< T > & khairullin::List< T >::operator=(const List & other) {
+  List< T > temp(other);
+  swap(temp);
+  return *this;
+}
+
+template< class T >
+khairullin::List< T >::List(List &&other) noexcept:
+  head(other.head),
+  fake(other.fake) {
+  other.head = nullptr;
+  other.fake = nullptr;
+}
+
+template< class T >
+khairullin::List< T > & khairullin::List< T >::operator=(List &&other) {
+  List< T > temp(std::move(other));
+  swap(temp);
+  return *this;
+}
+
+template< class T >
+void khairullin::List< T >::addBegin(const T &val) {
+  Node< T > *new_node = new Node< T >(val);
+  fake->next = new_node;
+  auto next = head;
+  head = new_node;
+  new_node->next = next;
+}
+
+template< class T >
+void khairullin::List< T >::addEnd(const T &val) {
+  Node< T > *new_node = new Node< T >(val);
+  if (head == nullptr) {
+    head = new_node;
+    fake->next = new_node;
+    return;
+  }
+  auto curr = head;
+  while (curr->next != nullptr) {
+    curr = curr->next;
+  }
+  curr->next = new_node;
+}
+
+template< class T >
+void khairullin::List< T >::insert(const T &val, const T &after) {
+  Node< T > *curr = head;
+  while (curr && curr->val != after) {
+    curr = curr->next;
+  }
+  if (curr == nullptr) {
+    throw std::logic_error("This value doesn't exist");
+  }
+  Node< T > *new_node = new Node< T >(val);
+  new_node->next = curr->next;
+  curr->next = new_node;
+}
+
+template< class T >
+void khairullin::List< T >::cut(const T &val) {
+  Node< T > *curr = head;
+  Node< T > *prev = fake;
+  while (curr && curr->val != val) {
+    prev = curr;
+    curr = curr->next;
+  }
+  if (curr == nullptr) {
+    throw std::logic_error("This value doesn't exist");
+  }
+  prev->next = curr->next;
+  curr->next = nullptr;
+  delete curr;
+}
+
+template< class T >
+void khairullin::List< T >::clear() {
+  ::operator delete[](fake);
+  while (head != nullptr) {
+    auto next = head->next;
+    delete head;
+    head = next;
+  }
+}
+
+template< class T >
+khairullin::ConstListIterator< T > khairullin::List< T >::сbegin() const {
+  return ConstListIterator(head);
+}
+
+template< class T >
+khairullin::ConstListIterator< T > khairullin::List< T >::сend() const
+{
+  return ConstListIterator< T >(nullptr);
+}
+
+template< class T >
+khairullin::ListIterator< T > khairullin::List< T >::begin() const
+{
+  return ListIterator< T >(head);
+}
+
+template< class T >
+khairullin::ListIterator< T > khairullin::List< T >::end() const
+{
+  return ListIterator< T >(nullptr);
+}
+
+template< class T >
+void khairullin::List< T >::swap(List & other) {
+  std::swap(head, other.head);
+  std::swap(fake->next, other.fake->next);
+}
+
+template< class T >
+khairullin::ListIterator< T >::ListIterator():
+  current(nullptr)
+{}
+
+template< class T >
+khairullin::ListIterator< T >::ListIterator(Node< T > * curr):
+  current(curr)
+{}
+
+template< class T >
+khairullin::ListIterator< T >::ListIterator(const ListIterator & other):
+  current(other.current)
+{}
+
+template< class T >
+khairullin::ListIterator< T > & khairullin::ListIterator< T >::operator=(const ListIterator & other) {
+  if (*this == other) {
+    return *this;
+  }
+  current = other.current;
+  return *this;
+}
+
+template< class T >
+khairullin::ListIterator< T >::ListIterator(ListIterator &&other):
+  current(other.current)
+{
+  other.current = nullptr;
+}
+
+template< class T >
+khairullin::ListIterator< T > & khairullin::ListIterator< T >::operator=(ListIterator &&other) {
+  if (*this == other) {
+    return *this;
+  }
+  current = other.current;
+  other.current = nullptr;
+  return *this;
+}
+
+template< class T >
+khairullin::ListIterator< T > khairullin::ListIterator< T >::operator++() {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator++() called on empty list");
+  }
+  current = current->next;
+  return *this;
+}
+
+template< class T >
+khairullin::ListIterator< T > khairullin::ListIterator< T >::operator++(int) {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator++() called on empty list");
+  }
+  auto copy = *this;
+  current = current->next;
+  return copy;
+}
+
+template< class T >
+T khairullin::ListIterator< T >::operator*() {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator*() called on empty list");
+  }
+  return current->val;
+}
+
+template< class T >
+void khairullin::ListIterator< T >::insert(const T &val) {
+  auto next = current->next;
+  auto new_node = new Node< T >(val);
+  current->next = new_node;
+  new_node->next = next;
+}
+
+template< class T >
+bool khairullin::ListIterator<T>::operator==(const ListIterator & other)
+{
+  return current == other.current;
+}
+
+template< class T >
+bool khairullin::ListIterator<T>::operator!=(const ListIterator & other)
+{
+  return !(*this == other);
+}
+
+template< class T >
+khairullin::ConstListIterator< T >::ConstListIterator():
+  current(nullptr)
+{}
+
+template< class T >
+khairullin::ConstListIterator< T >::ConstListIterator(Node< T > * curr):
+  current(curr)
+{}
+
+template< class T >
+khairullin::ConstListIterator< T >::ConstListIterator(const ConstListIterator & other):
+  current(other.current)
+{}
+
+template< class T >
+khairullin::ConstListIterator< T > & khairullin::ConstListIterator< T >::operator=
+  (const ConstListIterator & other) {
+  if (*this == other) {
+    return *this;
+  }
+  current = other.current;
+  return *this;
+}
+
+template< class T >
+khairullin::ConstListIterator< T >::ConstListIterator(ConstListIterator &&other):
+  current(other.current)
+{
+  other.current = nullptr;
+}
+
+template< class T >
+khairullin::ConstListIterator< T > & khairullin::ConstListIterator< T >::operator=(ConstListIterator &&other) {
+  if (*this == other) {
+    return *this;
+  }
+  current = other.current;
+  other.current = nullptr;
+  return *this;
+}
+
+template< class T >
+khairullin::ConstListIterator< T > khairullin::ConstListIterator< T >::operator++() {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator++() called on empty list");
+  }
+  current = current->next;
+  return *this;
+}
+
+template< class T >
+khairullin::ConstListIterator< T > khairullin::ConstListIterator< T >::operator++(int) {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator++() called on empty list");
+  }
+  auto copy = *this;
+  current = current->next;
+  return copy;
+}
+
+template< class T >
+T khairullin::ConstListIterator< T >::operator*() {
+  if (current == nullptr) {
+    throw std::logic_error("ListIterator::operator*() called on empty list");
+  }
+  return current->val;
+}
+
+template< class T >
+bool khairullin::ConstListIterator<T>::operator==(const ConstListIterator & other)
+{
+  return current == other.current;
+}
+
+template< class T >
+bool khairullin::ConstListIterator<T>::operator!=(const ConstListIterator & other)
+{
+  return !(*this == other);
 }
 #endif

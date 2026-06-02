e6c526a52e284e1e98945412c619594d02324419
BinaryKenny <khairullinvadim2006@gmail.com>
2026-06-02 11:39:43 +0300

    I added 2 libraries that was written before

---
+++ khairullin.vadim/F0/Slot.h
@@ -0,0 +1,67 @@
+#ifndef SLOT_H
+#define SLOT_H
+#include <string>
+namespace khairullin {
+    template < class T >
+    struct Slot {
+        T value;
+        std::string key;
+        bool Empty;
+        size_t PLS;
+
+        bool operator==(const Slot & x) const;
+        void swap(Slot & other);
+
+        Slot();
+        ~Slot() = default;
+        Slot(const T & value, const std::string & key);
+        Slot(const Slot & other);
+        Slot & operator=(const Slot & slot);
+    };
+}
+
+template< class T >
+khairullin::Slot< T >::Slot():
+value(),
+key(""),
+Empty(true),
+PLS(0)
+{}
+
+template< class T >
+khairullin::Slot< T >::Slot(const T & value, const std::string & key):
+value(value),
+key(key),
+Empty(false),
+PLS(0)
+{}
+
+template< class T >
+khairullin::Slot< T >::Slot(const Slot & other):
+value(other.value),
+key(other.key),
+Empty(other.Empty),
+PLS(other.PLS)
+{}
+
+template<class T>
+khairullin::Slot<T> & khairullin::Slot<T>::operator=(const Slot & slot)
+{
+    auto temp(slot);
+    swap(temp);
+    return *this;
+}
+
+template< class T >
+bool khairullin::Slot< T >::operator==(const Slot & x) const {
+    return (value == x.value && isEmpty == x.isEmpty && Tombstone == x.Tombstone);
+}
+
+template< class T >
+void khairullin::Slot< T >::swap(Slot & other) {
+    std::swap(value, other.value);
+    std::swap(key, other.key);
+    std::swap(isEmpty, other.isEmpty);
+    std::swap(Tombstone, other.Tombstone);
+}
+#endif
